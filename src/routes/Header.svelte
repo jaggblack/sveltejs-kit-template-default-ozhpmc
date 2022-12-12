077e7c5b2fcbf9182f1a1d5313b1e7f1835e6582
jaggblack <120414937+jaggblack@users.noreply.github.com>
2022-12-13 00:29:05 +0100

    Update Header.svelte

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,6 +3,6 @@
   <div class="w3-center">
   <h4>Site personnel</h4>
   <h1 class="w3-xxxlarge w3-animate-bottom">TEMMAR FAHD </h1>
-   <img src="http://www.monsite.com/monimage.jpg" alt="Mon image" width="300" height="200" align="right">
+  
   </div>
 </header>
